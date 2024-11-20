559697c23789064b8711ea94bc74e245b77a4c85
ganapati3 <124158856+ganapati3@users.noreply.github.com>
2024-11-20 21:27:26 +0530

    updated db config

--- src/utils/db.mjs
+++ src/utils/db.mjs
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,DB_PORT } = process.env;
 
 export const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-  port:3307
+  port:DB_PORT
 });
